fbfb4b235f451eedb958262559f2b48c381bce04
Claudia Sollinger <claudia.sollinger@gmail.com>
2022-10-01 08:22:31 +0200

    feat: first design of the app

---
+++ script.js
@@ -0,0 +1,12 @@
+const stateTodo = {
+    filter: "all",
+    todos: [
+        {desc: "Learn HTML",
+        done: true},
+        {desc: "Learn CSS",
+        done: false},
+        {desc: "Learn JavaScript",
+        done: false},
+    ]
+}
+
