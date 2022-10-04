1a6bab80f8c163e8890dac7d1c38177cd65875cc
Claudia Sollinger <claudia.sollinger@gmail.com>
2022-10-04 12:16:48 +0200

    feat: add and first js functions

--- script.js
+++ script.js
@@ -10,3 +10,55 @@ const stateTodo = {
     ]
 }
 
+const newTodo = document.querySelector("#new-todo");
+if (newTodo === null) {
+    console.error ("new-todo is not avaliable");
+}
+const addBtn = document.querySelector("#add-button");
+if (addBtn === null) {
+    console.error ("new-todo is not avaliable");
+}
+
+function renderTodos(){
+    const output = document.querySelector(".output");
+    output.innerHTML = "";
+
+    for (let todo of stateTodo.todos){
+        let toDoLi = document.createElement("li");
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.checked = todo.done;
+        checkbox.className = "checkbox"
+        toDoLi.appendChild(checkbox);
+
+        toDoLi.todoObj = todo;
+
+        const todoText = document.createTextNode(todo.desc);
+        toDoLi.appendChild(todoText);
+        console.log(toDoLi);
+        output.appendChild(toDoLi);
+    }
+}
+
+renderTodos();
+
+addBtn.addEventListener("click", addTodo);
+
+function addTodo (){
+    if (newTodo.value.length < 3){
+        return;
+    }
+    const ToDo = newTodo.value;
+    stateTodo.todos.push({desc: ToDo, done: false});
+    newTodo.value = "";
+    renderHTML();
+    console.log (stateTodo)
+}
+
+
+const list = document.querySelector(".output");
+
+list.addEventListener("change", function (e) {
+    const todo = e.target.parentElement.todoObj;
+    todo.done = e.target.checked;
+})
