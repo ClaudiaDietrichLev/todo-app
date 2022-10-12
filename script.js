dcf9d0846cddeb69084d8b796d278c51c8d5a21d
Claudia Sollinger <claudia.sollinger@gmail.com>
2022-10-12 18:31:13 +0200

    refactor

--- script.js
+++ script.js
@@ -1,47 +1,33 @@
 /*read state from localstorage*/
 
-const stateTodo = JSON.parse(localStorage.getItem("stateTodo")) || {filter: "filter-all", todos: []};
-console.log(stateTodo);
-
-
-
-const astateTodo = {
-    filter: "filter-all",
-    todos: [
-        {desc: "Learn HTML",
-        done: true},
-        {desc: "Learn CSS",
-        done: false},
-        {desc: "Learn JavaScript",
-        done: false},
-    ]
-}
-
-
+const stateTodo = JSON.parse(localStorage.getItem('stateTodo')) || {
+  filter: 'filter-all',
+  todos: [],
+};
 
 /*Todos in an array */
 let todos = stateTodo.todos;
 
 /*Select the Input-Field for new Todos*/
-const newTodo = document.querySelector("#new-todo");
+const newTodo = document.querySelector('#new-todo');
 if (newTodo === null) {
-    console.error ("new-todo is not avaliable");
+  console.error('new-todo is not avaliable');
 }
 
 /*select Add-Button*/
-const addBtn = document.querySelector("#add-button");
+const addBtn = document.querySelector('#add-button');
 if (addBtn === null) {
-    console.error ("add-button is not avaliable");
+  console.error('add-button is not avaliable');
 }
 
 /*select DeleteButton*/
-const deleteBtn = document.querySelector(".remove");
+const deleteBtn = document.querySelector('.remove');
 if (deleteBtn === null) {
-    console.error ("delete-button is not avaliable");
+  console.error('delete-button is not avaliable');
 }
 
 /*select die Gruppe der Filter*/
-const filterRadio = document.querySelector(".filter");
+const filterRadio = document.querySelector('.filter');
 
 /*Filter aus dem State-Array auslesen und auswählen*/
 setFilter();
@@ -49,106 +35,101 @@ setFilter();
 /*Die Todos werden gerendert*/
 renderTodos(todos);
 
-filterRadio.addEventListener("change", function (e){
-    const filterType = e.target.value;
-    stateTodo.filter = filterType;
-    saveStateTodo(stateTodo);
-    switch (filterType) {
-        case "filter-all":
-            renderTodos(todos);
-            break;
-        case "filter-open":
-            const todoOpen = todos.filter(todo => !todo.done);
-            renderTodos(todoOpen);
-            break;
-        case "filter-done":
-            const todoDone = todos.filter(todo => todo.done);
-            renderTodos(todoDone);
-            break;
-    }
+filterRadio.addEventListener('change', function (e) {
+  const filterType = e.target.value;
+  stateTodo.filter = filterType;
+  saveStateTodo(stateTodo);
+  switch (filterType) {
+    case 'filter-all':
+      renderTodos(todos);
+      break;
+    case 'filter-open':
+      const todoOpen = todos.filter((todo) => !todo.done);
+      renderTodos(todoOpen);
+      break;
+    case 'filter-done':
+      const todoDone = todos.filter((todo) => todo.done);
+      renderTodos(todoDone);
+      break;
+  }
 });
 
-addBtn.addEventListener("click", addTodo);
+addBtn.addEventListener('click', addTodo);
 
-deleteBtn.addEventListener("click", function (){
-    todos = todos.filter(todo => !todo.done);
-    renderTodos(todos);
-    stateTodo.todos = todos;
-    saveStateTodo(stateTodo)
-})
+deleteBtn.addEventListener('click', function () {
+  todos = todos.filter((todo) => !todo.done);
+  renderTodos(todos);
+  stateTodo.todos = todos;
+  saveStateTodo(stateTodo);
+});
 
-const list = document.querySelector("#todo-list");
+const list = document.querySelector('#todo-list');
 
-list.addEventListener("change", function (e) {
-    const todo = e.target.parentElement.parentElement.todoObj;
-    const label = e.target.parentElement;
-    const todoText = label.querySelector(".todo");
-    todo.done = e.target.checked;
-    if (todo.done){
-        todoText.classList.add('todo-done');
-    } else {
-        todoText.classList.remove('todo-done');
-    }
-    saveStateTodo(stateTodo);
-})
+list.addEventListener('change', function (e) {
+  const todo = e.target.parentElement.parentElement.todoObj;
+  const label = e.target.parentElement;
+  const todoText = label.querySelector('.todo');
+  todo.done = e.target.checked;
+  if (todo.done) {
+    todoText.classList.add('todo-done');
+  } else {
+    todoText.classList.remove('todo-done');
+  }
+  saveStateTodo(stateTodo);
+});
 
-
-
-
-function setFilter () {
-    const filter = "#" + stateTodo.filter;
-    const filterBtn = document.querySelector(filter);
-    filterBtn.checked = true;     
+function setFilter() {
+  const filter = '#' + stateTodo.filter;
+  const filterBtn = document.querySelector(filter);
+  filterBtn.checked = true;
 }
 
-function renderTodos(todos){
-    const output = document.querySelector("#todo-list");
-    output.innerHTML = "";
+function renderTodos(todos) {
+  const output = document.querySelector('#todo-list');
+  output.innerHTML = '';
 
-    for (let todo of todos){
-        let toDoLi = document.createElement("li");
+  for (let todo of todos) {
+    let toDoLi = document.createElement('li');
 
-        const label = document.createElement("label");
-        label.setAttribute("class","container-todos");
-        
-        const span = document.createElement("span");
-        span.setAttribute("class","checkmark");
-        
-        const checkbox = document.createElement("input");
-        checkbox.setAttribute("type","checkbox");
-        checkbox.checked = todo.done;
+    const label = document.createElement('label');
+    label.setAttribute('class', 'container-todos');
 
-        const todoText = document.createElement("span");
-        todoText.setAttribute("class", "todo");
-        if (checkbox.checked){
-            todoText.setAttribute("class", "todo todo-done");
-        }
-        todoText.innerText = todo.desc;
+    const span = document.createElement('span');
+    span.setAttribute('class', 'checkmark');
 
-        label.appendChild(todoText);
-        label.appendChild(checkbox);
-        label.appendChild(span);
-        
-        toDoLi.appendChild(label);
-        toDoLi.todoObj = todo;
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.checked = todo.done;
 
-        output.appendChild(toDoLi);
+    const todoText = document.createElement('span');
+    todoText.setAttribute('class', 'todo');
+    if (checkbox.checked) {
+      todoText.setAttribute('class', 'todo todo-done');
     }
+    todoText.innerText = todo.desc;
+
+    label.appendChild(todoText);
+    label.appendChild(checkbox);
+    label.appendChild(span);
+
+    toDoLi.appendChild(label);
+    toDoLi.todoObj = todo;
+
+    output.appendChild(toDoLi);
+  }
 }
 
-
-function addTodo (){
-    if (newTodo.value.length < 5){
-        return;
-    }
-    const ToDo = newTodo.value;
-    stateTodo.todos.push({desc: ToDo, done: false});
-    newTodo.value = "";
-    renderTodos(todos);
-    saveStateTodo(stateTodo);
+function addTodo() {
+  if (newTodo.value.length < 5) {
+    return;
+  }
+  const ToDo = newTodo.value;
+  stateTodo.todos.push({ desc: ToDo, done: false });
+  newTodo.value = '';
+  renderTodos(todos);
+  saveStateTodo(stateTodo);
 }
 
-function saveStateTodo (stateTodo){
-    localStorage.setItem("stateTodo", JSON.stringify(stateTodo));
-    console.log(stateTodo);
+function saveStateTodo(stateTodo) {
+  localStorage.setItem('stateTodo', JSON.stringify(stateTodo));
 }
